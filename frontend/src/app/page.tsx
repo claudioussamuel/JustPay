18e99e853ed899061cb72588effedae51ad02a8f
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-02 15:03:49 +0000

    Place the entire homPage in the page.tsx file

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,8 @@
+import HomePage from '@/components/pages/homeComponent'
 import React from 'react'
 
 function Page() {
-  return (
-    <div>
-       <h1 className='font-bowlby text-3xl'>Justpay</h1>
-       <p className='font-dmMono'>pay everything with ease</p>
-      </div>
-  )
+  return <HomePage/>
 }
 
 export default Page
