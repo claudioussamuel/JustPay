3b094d0eaebb40469c48a11b7c77c0ed05c41341
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-02 15:01:13 +0000

    Use localize data across dapp

---
+++ frontend/src/app/data.js
@@ -0,0 +1,94 @@
+import { LuArrowDownRight } from "react-icons/lu";
+
+
+export const navLinks=[
+    {name:'home',href:'/'},
+    {name:'profile',href:'/profile'},
+    {name:'payment', href:'/payment'},
+    {name:'Terms', href:'#'}
+]
+
+export const sociaLinks=[
+    {name:'discord', icon: <LuArrowDownRight className='h-6'/>,href:'#'},
+    {name:'twitter', icon:<LuArrowDownRight className='h-6'/>,href:'#'},
+    {name:'linkedIn', icon:<LuArrowDownRight className='h-6'/>, href:'#'},
+    {name:'Instagram', icon:<LuArrowDownRight className='h-6'/>, href:'#'}
+  ]
+ 
+  export const footerLinks=[
+    {name:'terms',href:"/"},
+    {name:'dashboard', href:"/dashboard"},
+    {name:'profile', href:"/profile"},
+    {name:'payment', href:"/payment"},
+    {name:'terms',href:"/aa"},
+    {name:'dashboard', href:"/cc"},
+    {name:'profile', href:"/pe"},
+    {name:'payment', href:"/pt"}
+  ]
+  
+  export const faqs = [
+    {
+     question:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+     hovered:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+     touched:false
+    },
+    {
+      question:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      hovered:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      touched:false
+     },
+     {
+      question:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      hovered:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      touched:false
+     },
+     {
+      question:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      hovered:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      touched:false
+     },
+     {
+      question:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      hovered:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      touched:false
+     },
+     {
+      question:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      hovered:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, explicabo?",
+      touched:false
+     }
+  ]
+  
+    
+ 
+export const exploreSection=[
+    {description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque expedita nam in repudiandae, quibusdam debitis iure dicta molestiae dolor quaerat.',
+      image:"/images/v1.jpg"},
+  
+    {description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque expedita nam in repudiandae, quibusdam debitis iure dicta molestiae dolor quaerat.',
+      image:"/images/v2.jpg"},
+  
+    {description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque expedita nam in repudiandae, quibusdam debitis iure dicta molestiae dolor quaerat.', 
+      image:"/images/v3.jpg"},
+  
+    {description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque expedita nam in repudiandae, quibusdam debitis iure dicta molestiae dolor quaerat.',
+       image:"/images/v4.jpg"}
+  ]
+  
+
+  export const recommend = [
+    { firstName: "Frinquilla",lastName:'Sempe', occupation:'UI/UX Designer', info:"loremhighest solid must be worked and info a", img:'/images/v1.jpg'},
+    { firstName: "February",lastName:'Sempe', occupation:'UI/UX Designer', info:"loremhighest solid must be worked and info a", img:'/images/v1.jpg' },
+    { firstName: "March",lastName:'Sempe', occupation:'UI/UX Designer', info:"loremhighest solid must be worked and info a", img:'/images/v1.jpg' },
+    { firstName: "April",lastName:'Sempe', occupation:'UI/UX Designer', info:"loremhighest solid must be worked and info a", img:'/images/v1.jpg'},
+    { firstName: "May", lastName:'Sempe', occupation:'UI/UX Designer', info:"loremhighest solid must be worked and info a", img:'/images/v1.jpg'},
+    { firstName: "June",lastName:'Sempe', occupation:'UI/UX Designer', info:"loremhighest solid must be worked and info a", img:'/images/v1.jpg' },
+  ]
+  
+  export const teamMembers=[
+    {firstName:'Kenneth',role:"frontend developer", connect:"#", image:'/images/ken.jpg', lastName:'danso'},
+    {firstName:'Claud', role:"blockchain developer",connect:"#", image:'/images/claud_img.jpg', lastName:'opoku'},
+    {firstName:'makafui', role:"ui/ux designer",connect:"#", image:'/images/moto.jpg', lastName:'richmond'}
+  ]
+  
+  
