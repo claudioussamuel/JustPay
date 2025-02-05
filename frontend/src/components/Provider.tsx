065e386ced8e6560a159b160b3d5dc9908a34fd5
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-05 11:01:37 +0000

    Exempt id keys from console

--- frontend/src/components/Provider.tsx
+++ frontend/src/components/Provider.tsx
@@ -3,6 +3,5 @@ import { ReactNode } from "react";
 import { PrivyProvider } from "@privy-io/react-auth";
 
 export function Providers({children}:{children:ReactNode}){
-    console.log(process.env.NEXT_PUBLIC_PRIVY_APP_ID);
     return <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}>{children}</PrivyProvider>
 }
