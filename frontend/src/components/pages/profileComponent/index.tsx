ab71ad291f16f25749f10b120bcf0843f1aeb897
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-05 11:03:36 +0000

    Text the code logic

--- frontend/src/components/pages/profileComponent/index.tsx
+++ frontend/src/components/pages/profileComponent/index.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client";
 
-function index() {
+import { useEffect, useState } from "react";
+import { readContractData } from "@/lib/integrations/viem/contract";
+
+type ContractNameData = [string, boolean] | null; 
+
+export default function ContractReader() {
+  const [data, setData] = useState<ContractNameData>(null);
+  
+  const userAddress:`0x${string}`="0x4a18a9Dc57C6faA65EDBA9333c510d26d4154ecF"; 
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const result = await readContractData(userAddress);
+        console.log("Fetched Data:", result);
+
+        if (result) {
+          setData(result); 
+        }
+      } catch (error) {
+        console.error("Error fetching contract data:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div>index</div>
-  )
+    <div>
+      <h1>Smart Contract Data</h1>
+      {data ? (
+        <p>
+          Name: {data[0]} <br />
+          Has Name: {data[1] ? "Yes" : "No"}
+        </p>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
-
-export default index
