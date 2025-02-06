e48943b4d49f650ef65eea3673b50ac462d9acc0
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-06 12:44:16 +0000

    Header on the Contact pages

---
+++ frontend/src/components/content/ContactInscription.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+    title:string;
+    description:string;
+}
+
+function ContactInscription({title,description}: Props) {
+  return (
+    <div className='border py-5 rounded-2xl px-3'>
+        <h1>{title}</h1>
+        <p>{description}</p>
+    </div>
+  )
+}
+
+export default ContactInscription
