f7a0df818b401d8135ab3dbda9c64d9c85bb2bf5
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-06 12:45:20 +0000

    Create a Component for holding ContactDetails to avoid blotting of code

---
+++ frontend/src/components/pages/contactComponent/EventContact.tsx
@@ -0,0 +1,15 @@
+import { eventsData } from '@/app/data'
+import ContactDescriptionEvents from '@/components/content/ContactDescriptionEvents'
+import React from 'react'
+
+function EventContact() {
+  return (
+    <div className='space-y-5'>
+        {eventsData.map((event,index)=>(
+            <ContactDescriptionEvents key={index} {...event}/>
+        ))}
+    </div>
+  )
+}
+
+export default EventContact
