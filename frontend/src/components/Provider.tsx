6469a324f039c144284d2917dfa9028a3562db4b
CLAUDIOUS OPOKU MENSAH <claudiouso00@gmail.com>
2025-02-08 12:26:17 +0000

    (feat) changes in the Provider

--- frontend/src/components/Provider.tsx
+++ frontend/src/components/Provider.tsx
@@ -3,5 +3,29 @@ import { ReactNode } from "react";
 import { PrivyProvider } from "@privy-io/react-auth";
 
 export function Providers({children}:{children:ReactNode}){
-    return <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}>{children}</PrivyProvider>
+    return <PrivyProvider 
+    config={{
+        loginMethods:[
+            "wallet",
+            "email",
+            "github",
+            "apple",
+            "tiktok",
+            "telegram",
+            "sms",
+            "twitter",
+            "discord",
+            "spotify",
+            "google",
+            "instagram",
+            "linkedin"
+        ],
+        embeddedWallets:{
+            createOnLogin:"users-without-wallets"
+        }
+        
+    }}
+    appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}>
+        {children}
+        </PrivyProvider>
 }
