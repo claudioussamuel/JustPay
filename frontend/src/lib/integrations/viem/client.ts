33c841785b141968b50cbc9a49d7469acbea7b4f
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-12 18:01:17 +0000

    Replace infura with alchemy rpc

--- frontend/src/lib/integrations/viem/client.ts
+++ frontend/src/lib/integrations/viem/client.ts
@@ -1,8 +1,14 @@
 import { createPublicClient, http } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import { baseSepolia } from "viem/chains";
+import { sepolia } from "viem/chains";
 
-const BASE_SEPOLIA=`https://base-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_BASE_SEPOLIA_ID}`;
+if (!process.env.NEXT_PUBLIC_BASE_SEPOLIA_ID) {
+  throw new Error("Missing NEXT_PUBLIC_BASE_SEPOLIA_ID in environment variables");
+}
+
+const BASE_SEPOLIA = `https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_SEPOLIA_ID}`;
+
+
 
 const PRIVATE_KEY = process.env.NEXT_PUBLIC_META_MASK_PRIVATE_KEY
   ? process.env.NEXT_PUBLIC_META_MASK_PRIVATE_KEY.startsWith("0x")
@@ -10,14 +16,9 @@ const PRIVATE_KEY = process.env.NEXT_PUBLIC_META_MASK_PRIVATE_KEY
     : (`0x${process.env.NEXT_PUBLIC_META_MASK_PRIVATE_KEY}` as `0x${string}`)
   : null;
 
-
-
-
 export const client = createPublicClient({
-    chain: baseSepolia,
-    transport: http(BASE_SEPOLIA),
-})
+  chain: sepolia,
+  transport: http(BASE_SEPOLIA),
+});
 
-
-
-export const account = PRIVATE_KEY ? privateKeyToAccount(PRIVATE_KEY) : null
+export const account = PRIVATE_KEY ? privateKeyToAccount(PRIVATE_KEY) : null;
