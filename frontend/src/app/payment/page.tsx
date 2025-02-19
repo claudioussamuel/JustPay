78f3df1939874c893156474f3c2378329105e58e
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-19 06:44:58 +0000

    refactor code logic

--- frontend/src/app/payment/page.tsx
+++ frontend/src/app/payment/page.tsx
@@ -1,7 +1,7 @@
 
 import Payment from '@/components/pages/paymentComponent'
 import ProtectedRoute from '@/hooks/ProtectedRoute'
-import React from 'react'
+
 
 function Page() {
   return (
---
+++ frontend/src/app/payment/requestPayment/page.tsx
@@ -0,0 +1,8 @@
+import RequestPaymentForm from '@/components/pages/paymentComponent/RequestPaymentForm'
+import React from 'react'
+
+function page() {
+  return <RequestPaymentForm/>
+}
+
+export default page
