8b38c2ca0e1766e6164935d997dfb8a75cac553e
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-19 16:53:12 +0000

    Implement a component for handling unavailable content

---
+++ frontend/src/components/unavailable/UnavailableData.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import React from 'react'
+import { UnavailabeDataType } from '../../../types/global.types'
+
+
+function UnavailableData({title,image,description}:UnavailabeDataType) {
+  return (
+    <div className='h-dvh'>
+        <div className='grid place-content-center place-self-center'>
+            <h1 className='font-bowlby text-zinc-800 text-3xl mb-5'>{title}</h1>
+
+            <div className='mb-5'>
+                <Image src={image} width={450} height={450} alt={`${image}.txt`}/>
+            </div>
+
+            <div>
+                <p className='font-dmMono text-zinc-800'>{description}</p>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default UnavailableData
