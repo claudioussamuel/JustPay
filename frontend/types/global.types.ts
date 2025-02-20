fee98d1459cdda0f0dcacdd70ac7419e96118fe6
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-20 19:23:49 +0000

    Create types for users

--- frontend/types/global.types.ts
+++ frontend/types/global.types.ts
@@ -3,3 +3,18 @@ export interface UnavailabeDataType  {
     description:string;
     image:string;
 }
+
+export interface User{
+    firstName:string;
+    lastName: string;
+    xHandle: string;
+    facebookHandle:string;
+    igHandle:string;
+    address:string;
+    occupation:string;
+}
+
+export interface UserContextType{
+    users: User[];
+    addUser:(user: User) =>void;
+}
