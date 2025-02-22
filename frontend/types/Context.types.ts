2057eec03c2e30626240e8d6445cb22bb5cf4a0c
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-22 15:00:12 +0000

    Types refactor

--- frontend/types/Context.types.ts
+++ frontend/types/Context.types.ts
@@ -4,3 +4,17 @@ export type PaginationContextType={
     setCurrentPage: (page: number | ((prev:number)=>number))=>void
     setItemsPerPage: (items:number)=>void
 }
+
+export type UsePaginationProps = {
+    itemsPerPage: number;
+    totalItems: number;
+}
+
+export type UsePaginationReturn = {
+    currentPage:number;
+    itemsPerPage: number;
+    setCurrentPage: (page:number)=>void;
+    setItemsPerPage: (items:number)=>void;
+    totalPages: number;
+    paginatedItems: <T>(items: T[])=>T[];
+}
