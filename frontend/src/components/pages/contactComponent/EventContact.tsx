d1e9b17eadd66714ccb4ea831d4b0d11fc174367
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-02-25 10:35:47 +0000

    Pass selected props using contextAPI

--- frontend/src/components/pages/contactComponent/EventContact.tsx
+++ frontend/src/components/pages/contactComponent/EventContact.tsx
@@ -1,13 +1,14 @@
+import { useSelectedContactContext } from '@/app/context/SelectContext'
 import { eventsData } from '@/app/data'
 import ContactDescriptionEvents from '@/components/content/ContactDescriptionEvents'
 import React from 'react'
 
+
 function EventContact() {
   return (
     <div className='space-y-5'>
-        {eventsData.map((event,index)=>( 
-            <ContactDescriptionEvents key={index} {...event}/>
-        ))}
+            <ContactDescriptionEvents />
+      
     </div>
   )
 }
